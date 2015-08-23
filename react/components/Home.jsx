46accdb3c091738cb595787d37dc16e22ad2b881
cctmmpeining <cctmmpeining@gmail.com>
2015-08-23 18:13:02 -0400

    test update

--- react/components/Home.jsx
+++ react/components/Home.jsx
@@ -9,15 +9,8 @@ var Home = React.createClass({
     //Diff: 1. class -> className; 2. for -> htmlFor; 3. <input ..... />;  4. <br> -> <br></br>
     //5. a single element to contain the render stuff
     return (
-      <div className="row">
-        <div className="col-sm-12">
-          <h2>YangFan Enrollment System</h2>
-          <div className="lead">
-            Welcome!  
-          </div>
-          <input type="submit" value="Submit"/>
-        </div>
-      </div>
+      <p>Hello</p>
+  {/*  comments */}
     );
   }
 });
