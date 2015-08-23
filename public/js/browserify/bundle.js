640d44aaaf2c235a29fc6a572768a4c1105211e8
Haoran <jiahaoran44@gmail.com>
2015-08-23 00:09:18 -0400

    react tutorial

--- public/js/browserify/bundle.js
+++ public/js/browserify/bundle.js
@@ -25714,15 +25714,20 @@ module.exports = GetStarted;
 
 var React=require('react');
 
+//Home is component created by React.JS
 var Home = React.createClass({displayName: "Home",
   render: function () {
+    //in render function, all the html/css are in the return brackets.
+    //Diff: 1. class -> className; 2. for -> htmlFor; 3. <input ..... />;  4. <br> -> <br></br>
+    //5. a single element to contain the render stuff
     return (
       React.createElement("div", {className: "row"}, 
         React.createElement("div", {className: "col-sm-12"}, 
           React.createElement("h2", null, "YangFan Enrollment System"), 
           React.createElement("div", {className: "lead"}, 
             "Welcome!"  
-          )
+          ), 
+          React.createElement("input", {type: "submit", value: "Submit"})
         )
       )
     );
--- react/components/Home.jsx
+++ react/components/Home.jsx
@@ -2,8 +2,12 @@
 
 var React=require('react');
 
+//Home is component created by React.JS
 var Home = React.createClass({
   render: function () {
+    //in render function, all the html/css are in the return brackets.
+    //Diff: 1. class -> className; 2. for -> htmlFor; 3. <input ..... />;  4. <br> -> <br></br>
+    //5. a single element to contain the render stuff
     return (
       <div className="row">
         <div className="col-sm-12">
@@ -11,6 +15,7 @@ var Home = React.createClass({
           <div className="lead">
             Welcome!  
           </div>
+          <input type="submit" value="Submit"/>
         </div>
       </div>
     );
