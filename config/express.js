61bbe5a1ae74db2d0c15862696cfc1214c9992ac
Haoran <jiahaoran44@gmail.com>
2015-09-09 22:40:35 -0400

    initial production mode

--- config/express.js
+++ config/express.js
@@ -54,7 +54,7 @@ module.exports = function (app, passport) {
 
   // Don't log during tests
   // Logging middleware
-  if (env !== 'test') app.use(morgan(log));
+  if (env !== 'test') app.use(morgan("combined", log));
 
   // Swig templating engine settings
   if (env === 'development' || env === 'test') {
